4abfa74de41299cb8641a92f165ee872eebacfc2
Ameya <ameyasurana10@gmail.com>
2023-10-15 18:02:35 +0530

    Image Uploading on Server and Retrieving(Multiple Files)

--- src/main/java/com/example/helloworld/ImageUpload.java
+++ src/main/java/com/example/helloworld/ImageUpload.java
@@ -15,24 +15,39 @@ import java.util.*;
 public class ImageUpload extends HttpServlet {
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Part filePart = request.getPart("file");
-        String fileName = filePart.getSubmittedFileName();
+        Collection<Part> fileParts = request.getParts();
+
         String uploadDirectory = "D:\\images";
-        String filePath = uploadDirectory + File.separator + fileName;
-        try (InputStream input = filePart.getInputStream();
-             OutputStream output = new FileOutputStream(filePath)) {
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = input.read(buffer)) > 0) {
-                output.write(buffer, 0, length);
+
+        for (Part filePart : fileParts) {
+            System.out.println(filePart.getName());
+            String fileName = getSubmittedFileName(filePart);
+
+            if (fileName != null) {
+                String filePath = uploadDirectory + File.separator + fileName;
+
+                try (InputStream input = filePart.getInputStream();
+                     OutputStream output = new FileOutputStream(filePath)) {
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = input.read(buffer)) > 0) {
+                        output.write(buffer, 0, length);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("File uploaded: " + filePath);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
-        System.out.println("File uploaded: " + filePath);
-        response.getWriter().print("The file uploaded successfully.");
-
-
+        response.getWriter().print("The files uploaded successfully.");
+    }
+    private String getSubmittedFileName(Part filePart) {
+        for (String contentDisposition : filePart.getHeader("content-disposition").split(";")) {
+            if (contentDisposition.trim().startsWith("filename")) {
+                return contentDisposition.substring(contentDisposition.indexOf('=') + 1).trim().replace("\"", "");
+            }
+        }
+        return null;
     }
 }
