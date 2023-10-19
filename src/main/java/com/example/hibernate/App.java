4cedb35599ded998fe00d86b2361c544060bea7a
Ameya <ameyasurana10@gmail.com>
2023-10-19 13:30:18 +0530

    HQL basic Queries

--- src/main/java/com/example/hibernate/App.java
+++ src/main/java/com/example/hibernate/App.java
@@ -10,7 +10,14 @@ public class App {
         Session session = factory.getCurrentSession();
         try{
             session.beginTransaction();
-           session.createQuery("update users set email = '1235@gmail.com' where username = 'Martin'").executeUpdate();
+            Users user = new Users();
+//            user.setEmail("qwerty@gmail.com");
+//            user.setUsername("hehe");
+//            user.setFirstName("qwer");
+//            user.setLastName("ty");
+//            session.save(user);
+            session.createQuery("update users set lastName = 'sex' where  userId=6").executeUpdate();
+            session.createQuery("delete from users where userId =  5").executeUpdate();
            session.getTransaction().commit();
         }finally {
             session.close();
