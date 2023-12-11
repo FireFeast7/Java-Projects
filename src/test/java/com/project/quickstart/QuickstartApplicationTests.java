eeb05a9e918ff34e3a9707c08bbaee67838f98fe
Ameya <ameyasurana10@gmail.com>
2023-12-11 19:39:59 +0530

    First Commit

---
+++ src/test/java/com/project/quickstart/QuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
