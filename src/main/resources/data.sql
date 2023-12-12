e3666bc4a92a1443c3ad6fa0569ee3a2c45c3e3c
Ameya <ameyasurana10@gmail.com>
2023-12-13 00:29:40 +0530

    Connectivity to Postgres Database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into widgets (id,name,purpose)
+values
+(1,'Widget A','Testing Purposes'),
+(2,'Widget B','Entertainment'),
+(3,'Widget C','Productivity');
+-- (4,"Widget D","Well-being");
+
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+drop table if exists "widgets";
+
+
+create table "widgets" (
+    "id" int primary key,
+    "name" text,
+    "purpose" text
+);
